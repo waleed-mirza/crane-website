714566f06e5a6d0beab8f0283239eac2ca3633aa
waleed mirza <waleedmirza667@gmail.com>
2022-04-28 21:26:37 +0500

    Image responsiveness in carousel

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document
   .querySelector(".hamburger .container")
   .addEventListener("click", (e) => {
-    e.target.classList.toggle("change");
+    // e.target.classList.toggle("change");
     document.querySelector(".sticky-navbar").classList.toggle("show");
     document.querySelector(".top-header").classList.toggle("show");
   });
